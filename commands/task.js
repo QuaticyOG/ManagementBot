d53e7a7999c10e5119cfb09942007c7fdd129b66
QuaticyOG <89028089+QuaticyOG@users.noreply.github.com>
2026-03-16 15:14:49 +0100

    Update task.js

--- commands/task.js
+++ commands/task.js
@@ -6,21 +6,49 @@ const { canCreateTasks } = require('../utils/permissions');
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('task')
-    .setDescription('Create a task')
+    .setDescription('Task commands')
+
     .addSubcommand((subcommand) =>
       subcommand
         .setName('create')
-        .setDescription('Open the task creation modal'),
+        .setDescription('Open the task creation modal')
+    )
+
+    .addSubcommand((subcommand) =>
+      subcommand
+        .setName('assign')
+        .setDescription('Assign a user to a task')
+        .addIntegerOption(option =>
+          option
+            .setName('task_id')
+            .setDescription('Task number')
+            .setRequired(true)
+        )
+        .addUserOption(option =>
+          option
+            .setName('user')
+            .setDescription('User to assign')
+            .setRequired(true)
+        )
     ),
 
   async execute(interaction) {
-    if (!canCreateTasks(interaction.member)) {
-      return interaction.reply({
-        embeds: [buildInfoEmbed('Access denied', 'You do not have permission to create tasks.', 0xed4245)],
-        ephemeral: true,
-      });
+
+    const subcommand = interaction.options.getSubcommand();
+
+    if (subcommand === 'create') {
+
+      if (!canCreateTasks(interaction.member)) {
+        return interaction.reply({
+          embeds: [buildInfoEmbed('Access denied', 'You do not have permission to create tasks.', 0xed4245)],
+          ephemeral: true,
+        });
+      }
+
+      await interaction.showModal(buildCreateTaskModal());
+      return;
     }
 
-    await interaction.showModal(buildCreateTaskModal());
+    // assign command handled in interactionCreate.js
   },
 };
