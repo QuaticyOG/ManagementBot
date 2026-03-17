9903f916df413d3abb09b5eeae36186998be942a
QuaticyOG <89028089+QuaticyOG@users.noreply.github.com>
2026-03-17 18:18:20 +0100

    Create trelloCalendar.js

---
+++ utils/trelloCalendar.js
@@ -0,0 +1,18 @@
+const axios = require('axios');
+
+const BASE = 'https://api.trello.com/1';
+
+async function getBoardLists() {
+  const res = await axios.get(`${BASE}/boards/${process.env.TRELLO_BOARD_ID}/lists`, {
+    params: {
+      key: process.env.TRELLO_KEY,
+      token: process.env.TRELLO_TOKEN,
+      cards: 'open',
+      card_fields: 'name'
+    }
+  });
+
+  return res.data;
+}
+
+module.exports = { getBoardLists };
