774511b22e03d18a1b88554c5e15ef77a74cac56
QuaticyOG <89028089+QuaticyOG@users.noreply.github.com>
2026-03-21 18:12:02 +0100

    Update trelloCalendar.js

--- utils/trelloCalendar.js
+++ utils/trelloCalendar.js
@@ -3,16 +3,31 @@ const axios = require('axios');
 const BASE = 'https://api.trello.com/1';
 
 async function getBoardLists() {
-  const res = await axios.get(`${BASE}/boards/${process.env.TRELLO_BOARD_ID}/lists`, {
+  const key = process.env.TRELLO_KEY;
+  const token = process.env.TRELLO_TOKEN;
+  const boardId = process.env.TRELLO_BOARD_ID;
+
+  const listsRes = await axios.get(`${BASE}/boards/${boardId}/lists`, {
+    params: { key, token }
+  });
+
+  const lists = listsRes.data;
+
+  const cardsRes = await axios.get(`${BASE}/boards/${boardId}/cards`, {
     params: {
-      key: process.env.TRELLO_KEY,
-      token: process.env.TRELLO_TOKEN,
-      cards: 'all',
-      card_fields: 'name'
+      key,
+      token,
+      fields: 'name,idList',
     }
   });
 
-  return res.data;
+  const cards = cardsRes.data;
+
+  for (const list of lists) {
+    list.cards = cards.filter(card => card.idList === list.id);
+  }
+
+  return lists;
 }
 
 module.exports = { getBoardLists };
